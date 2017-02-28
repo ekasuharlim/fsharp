81157c287be13702fcabb516b10ba591e1280c56
ekasuharlim <eka.suharlim>
2017-02-28 13:25:53 +0800

    lesson 3

--- DoThis/Program.cs
+++ DoThis/Program.cs
@@ -20,15 +20,22 @@ namespace WinTail
             //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
             // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-            var consoleWriter = MyActorSystem.ActorOf(Props.Create
-                (() => new ConsoleWriterActor()));
-            var consoleReader = MyActorSystem.ActorOf(Props.Create
-                (() => new ConsoleReaderActor(consoleWriter)));
+            //var consoleWriter = MyActorSystem.ActorOf(Props.Create
+            //    (() => new ConsoleWriterActor()));
+            //var consoleReader = MyActorSystem.ActorOf(Props.Create
+            //    (() => new ConsoleReaderActor(consoleWriter)));
 
-
-
+            Props consoleWriterProps = Props.Create(() => new ConsoleWriterActor());
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps,
+                "consoleWriterActor");
+            Props validationProp = Props.Create(() => new ValidationActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(validationProp,
+                   "validationActor");
+            Props consoleReaderProps = Props.Create(() => new ConsoleReaderActor(validationActor));
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps,
+                "consoleReaderActor");
             // tell console reader to begin
-            consoleReader.Tell(ConsoleReaderActor.StartCommand);
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
