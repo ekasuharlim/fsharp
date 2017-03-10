d2f141fe315c3cde96945b6b3362c92ac6d09706
ekasuharlim <eka.suharlim>
2017-03-10 13:21:21 +0800

    lesson 6

--- DoThis/TailActor.cs
+++ DoThis/TailActor.cs
@@ -50,25 +50,26 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
+
+        protected override void PreStart()
+        {
+            //base.PreStart();
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
+            _fileStream = new FileStream(Path.GetFullPath(_filePath),
+                FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
+            var text = _fileStreamReader.ReadToEnd();
+            Self.Tell(new InitialRead(_filePath, text));
+        }
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
-
-            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
-            _observer.Start();
-            _fileStream = new FileStream(Path.GetFullPath(_filePath),
-                FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
-            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
-            var text = _fileStreamReader.ReadToEnd();
-            Self.Tell(new InitialRead(_filePath, text));
-
-
-
         }
 
         protected override void OnReceive(object message)
@@ -91,6 +92,16 @@ namespace WinTail
                 _reporterActor.Tell(ir.Text);
             }
             
+        }
+
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            _fileStreamReader = null;
+            base.PostStop();
         }
     }
 }
